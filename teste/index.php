4213e01bfef91c92510472479d0349e010fb0c77
root <root@ubuntu-pc>
2017-02-07 00:44:21 -0300

    inicio

--- teste/index.php
+++ teste/index.php
@@ -5,9 +5,11 @@ $num = 0;
 $encontrou = false;
 while(!$encontrou){
     $hash = md5($num);
-    echo $hash."<br>";
-    if(substr($hash, 0,2) == "aa" || $num>=40000000){
+//    echo $num." - ".$hash."<br>";
+    if(substr($hash, 0,6) == "aaaaaa" || $num>=40000000){
         break;
     }
     $num++;
 }
+echo $num." - ".$hash."<br>";
+echo microtime(1)-$time;
