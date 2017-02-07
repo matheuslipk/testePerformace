8c80d0dda99c72f2399e8bd31365f210c1277e30
root <root@ubuntu-pc>
2017-02-07 01:00:11 -0300

    inicio

--- teste/index.php
+++ teste/index.php
@@ -1,11 +1,10 @@
 <?php
-
+ini_set("max_execution_time", '300');
 $time = microtime(1);
 $num = 0;
 $encontrou = false;
 while(!$encontrou){
     $hash = md5($num);
-//    echo $num." - ".$hash."<br>";
     if(substr($hash, 0,7) == "aaaaaaa" || $num>=120000000){
         break;
     }
